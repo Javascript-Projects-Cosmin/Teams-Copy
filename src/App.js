91565fa07d88555cdb70b09d8a880e4b948f056b
onitiucosmin <48385722+onitiucosmin@users.noreply.github.com>
2022-02-05 09:58:52 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '55ce2bd3-7096-4748-bf37-a109452baf16'; // Gotten from the Chat Engine API Project
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')} // The username you insert in the login screen
+      userSecret={localStorage.getItem('password')} // The password used, can also be done from the Chat Engine Platform
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
